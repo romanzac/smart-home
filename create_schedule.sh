1a50d9186c06a68a771b81d1cd6bcbf62de6aa5d
Roman <zajic@zajic.net>
2025-07-12 20:25:34 +0200

    fix: add schedule for MacOS

---
+++ create_schedule.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp com.roman.powestrip.on.plist ~/Library/LaunchAgents/
+cp com.roman.powestrip.off.plist ~/Library/LaunchAgents/
+
+launchctl load ~/Library/LaunchAgents/com.roman.powerstrip.on.plist
+launchctl load ~/Library/LaunchAgents/com.roman.powerstrip.off.plist
+
+launchctl list | grep com.roman.powerstrip
