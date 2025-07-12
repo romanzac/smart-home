5b91687ae548490d6703409cedbef8d3fa4db021
Roman <zajic@zajic.net>
2025-07-12 20:31:06 +0200

    fix: names

--- create_schedule.sh
+++ create_schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cp com.roman.powestrip.on.plist ~/Library/LaunchAgents/
-cp com.roman.powestrip.off.plist ~/Library/LaunchAgents/
+cp com.roman.powerstrip.on.plist ~/Library/LaunchAgents/
+cp com.roman.powerstrip.off.plist ~/Library/LaunchAgents/
 
 launchctl load ~/Library/LaunchAgents/com.roman.powerstrip.on.plist
 launchctl load ~/Library/LaunchAgents/com.roman.powerstrip.off.plist
